fc3a773f673bcdf0e851b5473cc310bab20d89bc
rt65s <rt65s@SCHOOL_LAPTOP>
2024-04-19 12:34:45 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Prog122_Assign_Password_Milender
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿//Charles Milender
+//4-18-2024
+//Programming 122
+//Assignment Password Login Form
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prog122_Assign_Password_Milender
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //create string variables for user input
+        public string userName;
+        public string password;
+        public MainWindow()
+        {
+            InitializeComponent();//<---Don't delete this
+            
+        }//end of MainWindow
+
+        private void btnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            //create variables for login user input from text boxes
+            string logInUserName = txtUserName.Text;
+            string logInPassword = txtPassWord.Text;
+            //test user input with if/else, first check username and password match stored username and password
+            if(logInUserName == userName && logInPassword == password)
+            {
+                //inform user of successful login
+                MessageBox.Show($"Login Successful");
+                //log user input
+                rtbDisplay.Text += $"Login Successful - Username: {logInUserName}, Password: {logInPassword}\n";
+                //clear text box fields
+                txtUserName.Text = "";
+                txtPassWord.Text = "";
+            }
+            //only runs if username and password don't match stored username and password
+            //inform user of invalid username or password
+            else
+            {
+                //inform user of invalid username or password
+                MessageBox.Show("Invalid username or password");
+                //log user input
+                rtbDisplay.Text += $"Sign In Attempt: Invalid username or password - Username: {logInUserName}, Password: {logInPassword}\n";
+            }
+            
+
+        }
+
+        private void btnSignUp_Click(object sender, RoutedEventArgs e)
+        {
+            //assign variables to user input from text boxes
+            userName = txtUserName.Text;
+            password = txtPassWord.Text;
+            //test user input with if, else if, else, first test for empty fields
+            if (userName.Length <= 0 || password.Length <= 0  )
+            {
+                //inform user to input valid user name and password
+                MessageBox.Show("Please enter a valid user name and password");
+                //log user input
+                rtbDisplay.Text = $"Sign up Attempt: Empty fields - Username:{userName} , Password:{password} \n";
+            }
+            //test user input for password: make sure it is at lest 8 charaters
+            else if (password.Length < 8)
+            {
+                //inform user password must be 8 charaters minimun
+                MessageBox.Show("The Password must be 8 charaters minimum");
+                //log user input
+                rtbDisplay.Text += $"Sign Up Attempt: Password too short - Username: {userName}, Password: {password} \n";
+            }
+            //inform user of successful creation of username and password
+            else
+            {
+                //inform user of successful sign up
+                MessageBox.Show("Sign Up Successful");
+                //log user input
+                rtbDisplay.Text = $"Sign Up Successful - Username: {userName}, Password: {password} \n";
+            }
+            //clear text box fields
+            txtUserName.Text = "";
+            txtPassWord.Text = "";
+        }
+    }//end of class
+
+}//end of namespace
